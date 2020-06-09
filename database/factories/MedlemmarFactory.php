16b2febb808f33959af7869058c55d6bf1f881eb
Kristoffer Hellenäs <kristoffer_hellenas@hotmail.se>
2020-06-09 10:13:11 +0200

    medlemmarseeder

--- database/factories/MedlemmarFactory.php
+++ database/factories/MedlemmarFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Medlemmar::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'birthday' => $faker->dateTimeBetween($startDate = '-80 years', $endDate = '-10 years', $timezone = null),
+        'email' => $faker->email,
+        'membership' => $faker->boolean($chanceOfGettingTrue = 80)
     ];
 });
--- database/seeds/MedlemmarSeeder.php
+++ database/seeds/MedlemmarSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class MedlemmarSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(MedlemmarSeeder::class);
+        factory(App\Medlemmar::class, 100)->create();
     }
 }

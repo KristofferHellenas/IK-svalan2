2cd3e9c8f3608236db9ba1ea02459c42ac8c7502
Kristoffer Hellenäs <kristoffer_hellenas@hotmail.se>
2020-06-09 09:55:20 +0200

    tables

---
+++ app/Http/Controllers/MedlemmarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Medlemmar;
+use Illuminate\Http\Request;
+
+class MedlemmarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Medlemmar  $medlemmar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Medlemmar $medlemmar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Medlemmar  $medlemmar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Medlemmar $medlemmar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Medlemmar  $medlemmar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Medlemmar $medlemmar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Medlemmar  $medlemmar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Medlemmar $medlemmar)
+    {
+        //
+    }
+}
---
+++ database/factories/MedlemmarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Medlemmar;
+use Faker\Generator as Faker;
+
+$factory->define(Medlemmar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

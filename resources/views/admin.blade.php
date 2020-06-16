24894195053e368a5cc2edd7acea4e79422aab37
Puttzon <puttzon@gmail.com>
2020-06-16 08:58:37 +0200

    La till margin i admin filen

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,29 +19,29 @@
   </div>
 
   <div class="row">
-    <div class="col">
+    <div class="col mt-3">
       <label for="exampleInputEmail1">Birthday</label>
       <input type="text" class="form-control" placeholder="ex. 1989-03-14">
     </div>
-    <div class="col">
+    <div class="col mt-3">
       <label for="exampleInputEmail1">Email</label>
       <input type="email" class="form-control" placeholder="ex. john@tests.com">
     </div>
   </div>
 
-   <div class="row">
-    <div class="col">
+  <div class="row">
+    <div class="col mt-3">
       <label for="exampleInputEmail1">Password</label>
       <input type="text" class="form-control" placeholder="Password">
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary mt-3">Submit</button>
 </form>
 
 <!-- Add team form -->
 <form action="/admin" method="POST">
   @csrf
-  <div class="row">
+  <div class="row mt-3">
     <div class="col">
       <label for="exampleInputEmail1">Add team</label>
       <input type="text" class="form-control" name="new_team">
@@ -51,10 +51,10 @@
       <input type="text" class="form-control" name="new_activity">
     </div>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary mt-3">Submit</button>
 </form>
 
-<table class="table">
+<table class="table mt-3">
   <thead>
     <tr>
       <th scope="col">Lag</th>
